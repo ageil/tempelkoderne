267c1c1547207348d5606be8d12b6bb0a1cf9608
Satannius <TOS@Tobiass-MacBook-Pro.local>
2016-11-04 12:38:40 +0100

    Indsat typer.

--- makeCode.fsx
+++ makeCode.fsx
@@ -1,1 +1,9 @@
-// Her kan vi teste funktionen makeCode.fsx
+// makeCode : player -> code
+
+type codeColor = Red | Green | Yellow | Purple | White | Black
+type code = codeColor list
+type answer = int * int
+type board = (code * answer) list
+type player = Human | Computer
+
+let testCode = [Red;Green;Yellow;White]
