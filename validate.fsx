4be51e5e214dcb66b75e73233dfa2def1886cbbb
Satannius <TOS@Tobiass-MacBook-Pro.local>
2016-11-14 18:14:03 +0100

    Fungerende validate. Ikke elegant, men det virker.

--- validate.fsx
+++ validate.fsx
@@ -1,2 +1,72 @@
 // validate : code -> code -> answer
 // som tager den skjulte opgave og et gæt og returnerer antallet af hvid og sort svarstifter.
+
+type codeColor = Red | Green | Yellow | Purple | White | Black
+type code = codeColor list
+type answer = int * int // sort * hvid
+type board = (code * answer) list
+type player = Human | Computer
+
+let sCode = [White; Red; Black; Black]
+let gCode = [Black; White; Red; Black]
+let toHistogram (c : code) =
+    let mutable r = 0
+    let mutable g = 0
+    let mutable y = 0
+    let mutable p = 0
+    let mutable w = 0
+    let mutable b = 0
+    for i = 0 to 3 do 
+        match c.[i] with
+        | Red -> r <- r + 1
+        | Green -> g <- g + 1
+        | Yellow -> y <- y + 1
+        | Purple -> p <- p + 1
+        | White -> w <- w + 1
+        | Black -> b <- b + 1
+    (r,g,y,p,w,b)
+
+                        // Secret tuple         guess tuple
+let compareHistograms (r1,g1,y1,p1,w1,b1) (r2,g2,y2,p2,w2,b2) =
+    let r =
+        if r2 <= r1 then r2
+        elif r2 > r1 then r1
+        else 0
+    let g =
+        if g2 <= g1 then g2
+        elif g2 > g1 then g1
+        else 0
+    let y =
+        if y2 <= y1 then y2
+        elif y2 > y1 then y1
+        else 0
+    let p =
+        if p2 <= p1 then p2
+        elif p2 > p1 then p1
+        else 0
+    let w =
+        if w2 <= w1 then w2
+        elif w2 > w1 then w1
+        else 0
+    let b =
+        if b2 <= b1 then b2
+        elif b2 > b1 then b1
+        else 0
+    (r+g+y+p+w+b)
+
+let validate (secretCode : code) (guessCode : code) =
+    let mutable blacks = 0
+    let mutable whites = 0
+    for i = 0 to 3 do                               // Optæller antallet af sorte stifter for hver rigtig farve i rigtig position.
+        if secretCode.[i] = guessCode.[i] then
+            blacks <- blacks + 1
+    let secretHistogram = (toHistogram secretCode)                          // Bestemmer et histogram over farverne i secretCode
+    let guessHistogram = (toHistogram guessCode)                            // Bestemmer et histogram over farverne i guessCode
+    let sumHistograms = (compareHistograms secretHistogram guessHistogram)  // Beregner antallet af "intersections" ml. de to histogrammer. Så hvis kode = 2 rød og gæt = 1x rød er antallet = 1. 
+    whites <- whites + sumHistograms
+    if whites > 0 then
+        whites <- whites - blacks
+    let result : answer = (blacks,whites)
+    result
+
+printfn "%A" (validate sCode gCode)
