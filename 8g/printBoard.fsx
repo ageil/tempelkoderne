aa7652cce6a43a96442fd6792c3dbcf79b147afe
Satannius <TOS@Tobiass-MacBook-Pro.local>
2016-11-15 16:32:36 +0100

    Tilføjet turn til boardet.

--- 8g/printBoard.fsx
+++ 8g/printBoard.fsx
@@ -9,8 +9,9 @@ type player = Human | Computer
 
 // Omdanner et board til en string, som kan printes.
 let printBoard (aBoard : board) =
-    let mutable stringBoard = (sprintf "%-10s%-10s%-10s%-10s %-5s\n----------------------------------------------\n" "Col1" "Col2" "Col3" "Col4" "B, W")
+    let mutable stringBoard = (sprintf "%-6s%-10s%-10s%-10s%-10s %-5s\n----------------------------------------------------\n" "Turn" "Col1" "Col2" "Col3" "Col4" "B, W")
     for i = 0 to aBoard.Length - 1 do   // Løber igennem hvert element, altså tuple, (code * answer) i et board.
+        stringBoard <- stringBoard + (sprintf "%-6d" (i+1))
         for j = 0 to 3 do               // Løber gennem det første element, en code, i hver tuple i board og skriver hvert element i code til stringBoard.
             stringBoard <- stringBoard + (sprintf "%-10s" (sprintf "%A" (fst (aBoard.[i])).[j]))
         stringBoard <- stringBoard + (sprintf "%-6s" (sprintf "%A" (snd (aBoard.[i])))) +  "\n" // Løber gennem det andet element, et answer, i hver tuple i board og skriver det til stringBoard
