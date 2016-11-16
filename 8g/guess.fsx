79c99bb5ad840e9389f66654c87174b5714c9633
Anders Geil <anders.geil@gmail.com>
2016-11-16 23:17:18 +0100

    Tilføjet en semi-intelligent humanGuess, der håndterer og retter gæt, og spørger ved tvivlstilfælde

--- 8g/guess.fsx
+++ 8g/guess.fsx
@@ -1,5 +1,5 @@
 // guess : player -> board -> code
-// som tager en spillertype, et bræt bestående af et spils tidligere gæt og svar og returnerer et nyt gæt enten ved input fra brugeren eller ved at programmet beregner et gæt.
+// som tager en spillertype, et bræt bestående af et spils tidligere gæt og svar og returnerer et nyt gæt enten ved input fra brugeren eller ved at programmet beregner et gæt.
 
 type codeColor = Red | Green | Yellow | Purple | White | Black
 type code = codeColor list
@@ -67,3 +67,37 @@ let guess (playerType : player) (currentBoard : board) =
 // Tester
 let testPlayer = Human
 // printfn "%A" (guess testPlayer testBoard)
+
+
+
+
+
+
+// Håndterer og retter human input. Spørger ved tvivlstilfælde.
+let humanGuess() =
+    let rec inputCode() =
+        printfn "Pick your colors!"
+        printfn "([R]ed; [G]reen; [Y]ellow; [P]urple; [W]hite; [B]lack)"
+        let input = System.Console.ReadLine().ToLower()
+        let sep = [|" "; "; "; ", "; ";"; ","; "-"|]
+        let splinput = input.Split (sep, System.StringSplitOptions.None)
+        if splinput.Length <> 4 then
+            printfn "You need to type 4 colors!"
+            inputCode ()
+        else
+            formatCode (Array.toList splinput)
+    and formatCode input =
+        match input with
+        | [] -> []
+        | h::t when h.[0] = 'r' -> Red::(formatCode t)
+        | h::t when h.[0] = 'g' -> Green::(formatCode t)
+        | h::t when h.[0] = 'y' -> Yellow::(formatCode t)
+        | h::t when h.[0] = 'p' -> Purple::(formatCode t)
+        | h::t when h.[0] = 'w' -> White::(formatCode t)
+        | h::t when h.[0] = 'b' -> Black::(formatCode t)
+        | h::t -> printfn "What did you mean by \"%s\"?" h;
+                  formatCode (System.Console.ReadLine().ToLower()::t)
+    inputCode()
+
+
+
