972dba36bda310f6af53ac3563533346208caf38
Satannius <TOS@Tobiass-MacBook-Pro.local>
2016-11-18 10:01:06 +0100

    Udkommenteret gl. kode. Redigeret return værdi.

--- 8g/guess.fsx
+++ 8g/guess.fsx
@@ -54,25 +54,6 @@ let endGame () =
 
 
 
-
-// guess : Tager en playertype og board for det nuværende spil og kalder en passende funktion, der genererer en kode.
-let guess (playerType : player) (currentBoard : board) =
-    if currentBoard.Length < 20 && playerType = Human then
-        enterCode ()
-    elif currentBoard.Length < 20 && playerType = Computer then
-        generateCode ()
-    else
-        endGame ()
-
-// Tester
-let testPlayer = Human
-// printfn "%A" (guess testPlayer testBoard)
-
-
-
-
-
-
 // Håndterer og retter human input. Spørger ved tvivlstilfælde.
 let humanGuess() =
     let rec inputCode() =
@@ -101,3 +82,19 @@ let humanGuess() =
 
 
 
+// guess : Tager en playertype og board for det nuværende spil og kalder en passende funktion, der genererer en kode.
+let guess (playerType : player) (currentBoard : board) =
+    if currentBoard.Length < 20 && playerType = Human then
+        let guess = humanGuess ()
+        
+    elif currentBoard.Length < 20 && playerType = Computer then
+        generateCode ()
+    else
+        endGame ()
+
+let testBoard = [([Red;Red;Red;Red],(0,0))]
+
+
+// Tester
+let testPlayer = Human
+printfn "%A" (guess testPlayer testBoard)
